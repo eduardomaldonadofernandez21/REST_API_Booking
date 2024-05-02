2f599ee2f96bcb2f36561cf1d0f73ac3fcbe4422
eduardomaldonadofernandez21 <edumaldonadofdez21@gmail.com>
2024-05-02 14:46:58 +0100

    Creation Author tests for method Post createAuthor

---
+++ src/test/java/com/devgucci/database/controllers/AuthorControllerIntegrationTests.java
@@ -0,0 +1,53 @@
+package com.devgucci.database.controllers;
+
+import com.devgucci.database.TestDataUtil;
+import com.devgucci.database.domain.entities.AuthorEntity;
+import com.devgucci.database.services.AuthorService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+public class AuthorControllerIntegrationTests {
+
+    private AuthorService authorService;
+
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public AuthorControllerIntegrationTests(MockMvc mockMvc, AuthorService authorService) {
+        this.mockMvc = mockMvc;
+        this.authorService = authorService;
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    public void testThatCreateAuthorSuccessfullyReturnsHttp201Created() throws Exception {
+        AuthorEntity testAuthorA = TestDataUtil.createTestAuthorEntityA();
+        testAuthorA.setId(null);
+        String authorJson = objectMapper.writeValueAsString(testAuthorA);
+
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/authors")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(authorJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        );
+    }
+}
---
+++ src/test/java/com/devgucci/database/controllers/BooksControllerIntegrationTests.java
@@ -0,0 +1,20 @@
+package com.devgucci.database.controllers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+public class BooksControllerIntegrationTests {
+
+    private MockMvc mockMvc;
+
+
+}
